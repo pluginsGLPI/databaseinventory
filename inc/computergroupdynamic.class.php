09fbdcd97be7af6ff4173871021520315bffd4d9
Stanislas <skita@teclib.com>
2025-11-18 14:18:52 +0100

    Get search on dynamic groups working (temp hack) (#90)
    * Get search on dynamic groups working (temp hack)
    
    * adapt changelog
    
    * fix CS
    
    * fix

--- inc/computergroupdynamic.class.php
+++ inc/computergroupdynamic.class.php
@@ -28,6 +28,8 @@
  * -------------------------------------------------------------------------
  */
 
+use function Safe\ob_start;
+use function Safe\ob_get_clean;
 use function Safe\preg_match;
 use function Safe\preg_replace;
 use function Safe\preg_split;
@@ -228,7 +230,12 @@ class PluginDatabaseinventoryComputerGroupDynamic extends CommonDBTM
             $p['actionname']   = 'save';
             $p['actionvalue']  = _sx('button', 'Save');
             $p['showbookmark'] = false;
+
+            //hack because submit button is not a submit... See https://github.com/glpi-project/glpi/pull/20731
+            ob_start();
             Search::showGenericSearch(Computer::getType(), $p);
+            $generic_search = ob_get_clean();
+            echo preg_replace('/type="button" name="save"/', 'type="submit" name="save"', $generic_search);
 
             //display result from search
             if (!$firsttime) {
